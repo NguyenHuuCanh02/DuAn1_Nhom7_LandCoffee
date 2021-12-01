64f5950e8a0a95101ffb6efb70e6a1e714f82f2a
dungntph17846 <94193912+dungntph17846@users.noreply.github.com>
2021-12-01 14:43:15 +0700

    dung_QL

---
+++ LandCoffee_Dev/build/classes/MDI/DuAn1_Nhom7_LandCoffee/DuAn1_Nhom7_LandCoffee/LandCoffee_Sql/Sql_BanGhi1.sql
@@ -0,0 +1,17 @@
+﻿select *from sanpham
+select *from khachhang
+select *from hoadon
+select *from hoadonchitiet
+insert into SANPHAM values ('SP003',N'Cà phê cốt dừa',30000,'d','NV001')
+insert into SANPHAM values ('SP004',N'Cà phê trứng',30000,'d','NV001')
+insert into hoadon values
+(150000,2,'NV001'),
+(90000,1,'NV001')
+insert into hoadonchitiet values 
+('SP002',2,150000,1,'11/10/2021'),
+('SP003',2,150000,2,'11/10/2021'),
+('SP004',2,150000,2,'11/10/2021')
+insert into hoadonchitiet values 
+('SP002',3,90000,1,'11/10/2021'),
+('SP003',3,90000,1,'11/10/2021'),
+('SP004',3,90000,1,'11/10/2021')
